5ab6363263fbe7d83e7413ae431ec834945e40e6
bearomorphism <bear890707@gmail.com>
2022-05-10 23:43:57 +0800

    add login form (#1)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Form, Button, InputGroup } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const login = ({ username }) => {
+    console.log(username);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>CNL 2022 Final</h1>
+      <Container>
+        <Form onSubmit={handleSubmit(login)}>
+          <InputGroup>
+            <Form.Control placeholder="Enter your username" {...register('username', { required: true })} />
+            <Button type="submit">Log in!</Button>
+          </InputGroup>
+          {errors.username && <Form.Text>You need to input your name</Form.Text>}
+        </Form>
+      </Container>
     </div>
   );
 }
