1008e00f893fc7e52c40a7cff9a97f59dc977151
nicks96432 <nicks96432@gmail.com>
2022-05-12 17:47:13 +0800

    add prettier, git hook and webpack (#3)

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import { Container, Form, Button, InputGroup, Row, Col } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CreateEvent from './components/CreateEvent';
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import CreateEvent from "./components/CreateEvent";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const login = ({ username }) => {
-    console.log(username);
-    console.log(errors);
-  }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+    const login = ({ username }) => {
+        console.log(username);
+        console.log(errors);
+    };
 
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>CNL 2022 Final</h1>
-      <Container>
-        <Row>
-          <Col>
-            <Form onSubmit={handleSubmit(login)}>
-              <InputGroup>
-                <Form.Control placeholder="Enter your username" {...register('username', { required: true })} />
-                <Button type="submit">Log in!</Button>
-              </InputGroup>
-              {errors.username && <Form.Text>You need to input your name</Form.Text>}
-            </Form>
-          </Col>
-          <Col>
-            <CreateEvent></CreateEvent>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>CNL 2022 Final</h1>
+            <Container>
+                <Row>
+                    <Col>
+                        <Form onSubmit={handleSubmit(login)}>
+                            <InputGroup>
+                                <Form.Control
+                                    placeholder="Enter your username"
+                                    {...register("username", {
+                                        required: true
+                                    })}
+                                />
+                                <Button type="submit">Log in!</Button>
+                            </InputGroup>
+                            {errors.username && (
+                                <Form.Text>
+                                    You need to input your name
+                                </Form.Text>
+                            )}
+                        </Form>
+                    </Col>
+                    <Col>
+                        <CreateEvent></CreateEvent>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/CreateEvent.js
+++ client/src/components/CreateEvent.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import {
-  Container,
-  Form,
-  Button,
-  InputGroup,
-  Row,
-  Col,
-  DropdownButton,
-  Dropdown
+    Container,
+    Form,
+    Button,
+    InputGroup,
+    Row,
+    Col,
+    DropdownButton,
+    Dropdown
 } from "react-bootstrap";
 import "../App.css";
 import "react-date-range/dist/styles.css"; // main style file
@@ -17,68 +17,88 @@ import { DateRangePicker } from "react-date-range";
 const DATE_EXTENSION = 7;
 
 const CreateEvent = () => {
-  let startDate = new Date(), endDate = new Date();
-  const selectionRange = {startDate: startDate, endDate: endDate, key: "selection"};
-  const [username, setUsername] = useState("");
-  const [dateRange, setDateRange] = useState(selectionRange); 
-  const [startTime, setStartTime] = useState(0);
-  const [endTime, setEndTime] = useState(24);
+    let startDate = new Date(),
+        endDate = new Date();
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection"
+    };
+    const [username, setUsername] = useState("");
+    const [dateRange, setDateRange] = useState(selectionRange);
+    const [startTime, setStartTime] = useState(0);
+    const [endTime, setEndTime] = useState(24);
 
-  const [logged, setLogged] = useState(false);
+    const [logged, setLogged] = useState(false);
 
-  let time_choices = [...Array(25).keys()];
-  let handleStartTime = (time) => {
-    console.log(`Start time: ${time}`);
-    setStartTime(time);
-  };
-  let handleEndTime = (time) => {
-    console.log(`End time: ${time}`);
-    setEndTime(time);
-  };
+    let time_choices = [...Array(25).keys()];
+    let handleStartTime = time => {
+        console.log(`Start time: ${time}`);
+        setStartTime(time);
+    };
+    let handleEndTime = time => {
+        console.log(`End time: ${time}`);
+        setEndTime(time);
+    };
 
-  let handleDate = ({selection}) => {
-    console.log(selection);
-    setDateRange({
-      startDate: selection.startDate,
-      endDate: selection.endDate,
-      key: selection.key
-    });
-    console.log(`Current Date Range:`, dateRange);
-  }
+    let handleDate = ({ selection }) => {
+        console.log(selection);
+        setDateRange({
+            startDate: selection.startDate,
+            endDate: selection.endDate,
+            key: selection.key
+        });
+        console.log(`Current Date Range:`, dateRange);
+    };
 
+    return (
+        <div className="App">
+            <div className="App-title">
+                <h1>Create new event</h1>
+            </div>
+            <div>
+                <Form>
+                    <Row>
+                        <Form.Label>Event Name</Form.Label>
+                        <InputGroup placeholder="Event Name"></InputGroup>
 
-  return (
-    <div className="App">
-      <div className="App-title">
-        <h1>Create new event</h1>
-      </div>
-      <div>
-        <Form>
-          <Row>
-            <Form.Label>Event Name</Form.Label>
-            <InputGroup placeholder="Event Name"></InputGroup>
-
-            <Form.Label>What dates might work?</Form.Label>
-            <InputGroup>
-              <DateRangePicker minDate={startDate} ranges={[dateRange]} onChange={handleDate}></DateRangePicker>
-            </InputGroup>
-            <Button variant="outline-secondary">Today</Button>
-          </Row>
-         <Form.Label>What times might work?</Form.Label>
-          <DropdownButton id="dropdown-basic-button" title="No earlier than:" onSelect={handleStartTime}>
-            {time_choices.map((time) => (
-              <Dropdown.Item eventKey={time}>{time}:00</Dropdown.Item>
-            ))}
-          </DropdownButton>
-          <DropdownButton id="dropdown-basic-button" title="No later than:" onSelect={handleEndTime}>
-            {time_choices.map((time) => (
-              <Dropdown.Item eventKey={time}>{time}:00</Dropdown.Item>
-            ))}
-          </DropdownButton>
-        </Form>
-      </div>
-    </div>
-  );
+                        <Form.Label>What dates might work?</Form.Label>
+                        <InputGroup>
+                            <DateRangePicker
+                                minDate={startDate}
+                                ranges={[dateRange]}
+                                onChange={handleDate}
+                            ></DateRangePicker>
+                        </InputGroup>
+                        <Button variant="outline-secondary">Today</Button>
+                    </Row>
+                    <Form.Label>What times might work?</Form.Label>
+                    <DropdownButton
+                        id="dropdown-basic-button"
+                        title="No earlier than:"
+                        onSelect={handleStartTime}
+                    >
+                        {time_choices.map(time => (
+                            <Dropdown.Item eventKey={time}>
+                                {time}:00
+                            </Dropdown.Item>
+                        ))}
+                    </DropdownButton>
+                    <DropdownButton
+                        id="dropdown-basic-button"
+                        title="No later than:"
+                        onSelect={handleEndTime}
+                    >
+                        {time_choices.map(time => (
+                            <Dropdown.Item eventKey={time}>
+                                {time}:00
+                            </Dropdown.Item>
+                        ))}
+                    </DropdownButton>
+                </Form>
+            </div>
+        </div>
+    );
 };
 
 export default CreateEvent;
