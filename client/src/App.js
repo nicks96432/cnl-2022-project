642741d5d2a87a22002ddfe17d62a3b35d7ef278
Jeff <jeff999955@gmail.com>
2022-05-12 10:27:59 +0800

    Create Event Only

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Form, Button, InputGroup } from 'react-bootstrap';
+import { Container, Form, Button, InputGroup, Row, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateEvent from './components/CreateEvent';
 
 function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const login = ({ username }) => {
     console.log(username);
+    console.log(errors);
   }
 
   return (
     <div className="App">
       {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -28,13 +29,20 @@ function App() {
       </header> */}
       <h1>CNL 2022 Final</h1>
       <Container>
-        <Form onSubmit={handleSubmit(login)}>
-          <InputGroup>
-            <Form.Control placeholder="Enter your username" {...register('username', { required: true })} />
-            <Button type="submit">Log in!</Button>
-          </InputGroup>
-          {errors.username && <Form.Text>You need to input your name</Form.Text>}
-        </Form>
+        <Row>
+          <Col>
+            <Form onSubmit={handleSubmit(login)}>
+              <InputGroup>
+                <Form.Control placeholder="Enter your username" {...register('username', { required: true })} />
+                <Button type="submit">Log in!</Button>
+              </InputGroup>
+              {errors.username && <Form.Text>You need to input your name</Form.Text>}
+            </Form>
+          </Col>
+          <Col>
+            <CreateEvent></CreateEvent>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
