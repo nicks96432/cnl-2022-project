428f7be133601146757304910d79b8b914295956
龍品瑞 <nicks96432@gmail.com>
2022-05-13 10:57:38 +0800

    change tab size to 2

--- client/src/App.js
+++ client/src/App.js
@@ -6,46 +6,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
-    const login = ({ username }) => {
-        console.log(username);
-        console.log(errors);
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+  const login = ({ username }) => {
+    console.log(username);
+    console.log(errors);
+  };
 
-    return (
-        <div className="App">
-            <h1>CNL 2022 Final</h1>
-            <Container>
-                <Row>
-                    <Col>
-                        <Form onSubmit={handleSubmit(login)}>
-                            <InputGroup>
-                                <Form.Control
-                                    placeholder="Enter your username"
-                                    {...register("username", {
-                                        required: true
-                                    })}
-                                />
-                                <Button type="submit">Log in!</Button>
-                            </InputGroup>
-                            {errors.username && (
-                                <Form.Text>
-                                    You need to input your name
-                                </Form.Text>
-                            )}
-                        </Form>
-                    </Col>
-                    <Col>
-                        <CreateEvent></CreateEvent>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>CNL 2022 Final</h1>
+      <Container>
+        <Row>
+          <Col>
+            <Form onSubmit={handleSubmit(login)}>
+              <InputGroup>
+                <Form.Control
+                  placeholder="Enter your username"
+                  {...register("username", {
+                    required: true
+                  })}
+                />
+                <Button type="submit">Log in!</Button>
+              </InputGroup>
+              {errors.username && (
+                <Form.Text>You need to input your name</Form.Text>
+              )}
+            </Form>
+          </Col>
+          <Col>
+            <CreateEvent></CreateEvent>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
