474627847e08cbf018f6031eac4830c5d098f2d6
龍品瑞 <nicks96432@gmail.com>
2022-06-01 12:00:22 +0800

    finalize

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -8,11 +8,10 @@ import "./Login.css";
 const Login = () => {
   const navigate = useNavigate();
   const { setUserId } = useContext(Context);
-  const [isLogin, setIsLogin] = useState(true);
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = async () => {
+  const login = async isLogin => {
     let response;
     try {
       response = await axios.post(
@@ -22,7 +21,8 @@ const Login = () => {
         { name, password }
       );
     } catch (e) {
-      console.error(`${isLogin ? "login" : "signup"} failed: ${e}`);
+      if (isLogin) alert("Log in failed: username or password incorrect");
+      else alert("Sign up failed: account exists");
       return;
     }
     setUserId(response.data._id);
@@ -47,11 +47,19 @@ const Login = () => {
           />
         </div>
         <div className="Login__input-group">
-          <button type="submit" className="Login__button" onClick={login}>
-            {isLogin ? "ログイン!" : "サインアップ!"}
+          <button
+            type="submit"
+            className="Login__button"
+            onClick={() => login(true)}
+          >
+            Log in
           </button>
-          <button className="Login__button" onClick={e => setIsLogin(s => !s)}>
-            スウィッチ!
+          <button
+            type="submit"
+            className="Login__button"
+            onClick={() => login(false)}
+          >
+            Sign up
           </button>
         </div>
       </form>
